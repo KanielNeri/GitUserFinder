d5355319cedc24af3c4a418897f3fd02753c47d4
KanielNeri <itatarious@icloud.com>
2023-04-27 21:46:02 +0500

    first

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  publicPath: '/GitUserFinder/',
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          oneOf: [
+            {
+              resourceQuery: /^\?vue/,
+              use: ['pug-plain-loader']
+            },
+            {
+              use: ['raw-loader', 'pug-plain-loader']
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
